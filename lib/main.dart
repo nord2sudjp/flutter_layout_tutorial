2117c6d628e527b4e125094173837e3b1580a03b
maka y <nord2sudjp@gmail.com>
2020-11-07 19:31:06 +0900

    Expand

--- lib/main.dart
+++ lib/main.dart
@@ -12,26 +12,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Material Design',
-      home: Center(
-        child: Container(
-          color: Colors.white,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-
-            //mainAxisSize: MainAxisSize.max,
-
-            mainAxisAlignment: MainAxisAlignment.center,
-
-            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Container( color: Colors.blue, width: 50, height:50 ),
-              Container( color: Colors.red, width: 50, height:50 ),
-              Container( color: Colors.green, width: 50, height:50 ),
-              Container( color: Colors.orange, width: 50, height:50 ),
-            ],
-          ),
-
+      home: Container(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+                child:Container( color: Colors.blue)
+            ),
+            Expanded(
+              flex:2,
+              child:Container( color: Colors.red),
+            ),
+            /*
+            Expanded(
+              child:Container( color: Colors.green),
+            ),
+            */
+            Expanded(
+              child:Container( color: Colors.orange),
+            ),
+          ],
         ),
       ),
     );
